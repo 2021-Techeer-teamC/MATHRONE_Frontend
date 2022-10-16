7824dc1ffc2fed61bf55414a79fbaa1efb2a9c3c
YujeongLEE <dldbwjd24@ewhain.net>
2022-10-16 20:39:23 +0900

    feat : domain google login dto

---
+++ src/types/googleAccessDTOtmp.ts
@@ -0,0 +1,8 @@
+export interface googleAccessDTOtmp {
+    accessToken: string,
+    expireIn : string,
+    refreshToken : string,
+    scope : string,
+    tokenType : string,
+    idToken : string
+}
