e92408d697891b7e3b244f736b1347d27f7efbd4
hyunwoo <ashyonu@gmail.com>
2022-11-25 00:56:11 +0900

    fix: Fix typo

--- src/services/problemsService.ts
+++ src/services/problemsService.ts
@@ -3,13 +3,15 @@ import axios from "axios";
 
 class problemsService {
     getProblems(workbookId : string|undefined, chapterId: string|undefined){
+        console.log(workbookId, chapterId);
         return axios.get<problems[]>(
-            "http://localhost:8080/problem/detail-page/all?workbookid=${workbookId}&chapterid=${chapterId}", // api 주소
+            `http://localhost:8080/problem/detail-page/all?workbookId=${workbookId}&chapterId=${chapterId}`, // api 주소
+            //"http://localhost:8080/problem/detail-page/all?workbookId=02&chapterId=01",
             {
                 headers: {
                   "Content-Type": "application/json",
-                  "Accept": "application/json",
-                  "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
+                  "Accept": "application/json"
+                  //"Authorization": `Bearer ${localStorage.getItem("accessToken")}`
                 },
             }
         )
