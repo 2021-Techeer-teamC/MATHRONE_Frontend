13bff419e03149ae23ec54263528d01871da1001
YujeongLEE <dldbwjd24@ewhain.net>
2022-12-27 23:46:34 +0900

    feat : kakao sns login 결과 데이터 형식

---
+++ src/types/kakaoAccessDTO.ts
@@ -0,0 +1,9 @@
+export interface kakaoAccessDTO {
+    tokenType:string,
+    accessToken:string,
+    idToken:string,
+    expiresIn:number,
+    refreshToken:string,
+    refreshTokenExpiresIn:number,
+    scope:string
+}
