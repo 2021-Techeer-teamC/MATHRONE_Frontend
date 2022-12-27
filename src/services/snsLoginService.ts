df15bba4f2d9d4eeea7a4b1a4cd42cf7d2dff214
YujeongLEE <dldbwjd24@ewhain.net>
2022-12-27 23:44:55 +0900

    feat : kakao login service 정의

--- src/services/snsLoginService.ts
+++ src/services/snsLoginService.ts
@@ -3,21 +3,11 @@ import {signInUserItem} from "../types/userItem";
 import qs from "qs";
 import bookItem from "../types/bookItem";
 import {googleAccessDTOtmp} from "../types/googleAccessDTOtmp";
+import {kakaoAccessDTO} from "../types/kakaoAccessDTO";
 
 class snsLoginService {
     //Signin.tsx
-    // signInWithGoogle(code : null | string ){
-    //     // @ts-ignore
-    //     return axios.post<signInUserItem>(
-    //         `/user/signin/google?code=${code}`,
-    //         {
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //                 "Accept": "application/json",
-    //             },
-    //         }
-    //     )
-    // }
+
 
     //get방식 안됨 data parpametre로 보내는 방식으로는 넘어가지 않음 ?code=${code} : nono
     signInWithGoogle( code : null | string ){
@@ -32,5 +22,20 @@ class snsLoginService {
             }
         );
     }
+
+
+    signInWithKakao( code : null | string ){
+        return axios.post<kakaoAccessDTO>(
+            "http://localhost:8080/user/oauth/callback/kakao",
+            { 'code': code },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                },
+            }
+        );
+    }
+
 }
 export default new snsLoginService();
