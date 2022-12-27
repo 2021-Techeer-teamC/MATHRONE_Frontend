37fa99ae5a89d13fb800483995ff73a647cff9e5
YujeongLEE <dldbwjd24@ewhain.net>
2022-12-27 23:46:07 +0900

    feat : sns login redirect page 추가

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ import SignUp from "./pages/User/SignUp.tsx";
 import Profile from "./pages/Profile";
 import ProblemDetail from './pages/ProblemDetail';
 import Result from './pages/Result';
-import Oauth from './pages/Oauth/Oauth2RedirectLoading';
+import OauthGoogle from './pages/Oauth/Oauth2RedirectLoading';
+import OauthKakao from './pages/Oauth/Oauth2KakaoRedirect';
 
 const sections = [
   { title: "소개", url: "/info" },
@@ -36,7 +37,8 @@ function App() {
             <Route path="/books" exact element={<Books sections={sections}/>} />
             <Route path="/books/:id" element={<BookDetail sections={sections}/>} />
             <Route path="/rank" exact element={<Rank sections={sections}/>} />
-            <Route path="/oauth/callback/google" exact element={<Oauth sections={sections}/>} />
+            <Route path="/oauth/callback/google" exact element={<OauthGoogle sections={sections}/>} />
+            <Route path="/oauth/callback/kakao" exact element={<OauthKakao sections={sections}/>} />
             <Route path="/problem/:workbookId/:chapterId" element={<ProblemDetail sections={sections}/>} /> {/*임시 테스트용*/}
             <Route path="/result" exact element={<Result sections={sections}/>} /> {/*임시 테스트용*/}
           </Routes>
