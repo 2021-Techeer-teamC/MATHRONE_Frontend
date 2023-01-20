9caa7cd233c21a7cc26ceff37fac58064a2cc323
YujeongLEE <dldbwjd24@ewhain.net>
2023-01-21 01:37:25 +0900

    feat : tokenDto data type변경

--- src/types/userItem.ts
+++ src/types/userItem.ts
@@ -8,15 +8,10 @@ export interface signInUserItem {
     accountId: string;
   };
   snsInfo :  {
-    tokenType:string,
-    accessToken:string,
-    idToken:string,
-    expiresIn:number,
-    refreshToken:string,
-    refreshTokenExpiresIn:number,
-    scope:string;
+    snsAccessToken:string,
   };
 }
+
 
 export interface signUpUserItem {
   accountId: string;
