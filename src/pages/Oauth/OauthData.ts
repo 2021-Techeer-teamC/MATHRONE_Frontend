0dc5b7b7820ba0a281f8e35bbf90395f6801e7e9
YujeongLEE <dldbwjd24@ewhain.net>
2023-01-21 01:40:54 +0900

    fixed : client id secret처리 & google refresh Token 받기

--- src/pages/Oauth/OauthData.ts
+++ src/pages/Oauth/OauthData.ts
@@ -1,15 +1,15 @@
 import qs from "qs";
 
-const CLIENT_ID = "510387731827-nn7phkh1dd5oi8fdvjc7g5dv0sdq28i7.apps.googleusercontent.com";
+const CLIENT_ID = "[client ID]";
 const REDIRECT_URI = "http://localhost:3000/oauth/callback/google";
 const SCOPE = "https://www.googleapis.com/auth/userinfo.profile,https://www.googleapis.com/auth/userinfo.email".replaceAll(","," ");
 
 //google 인증
-export const GOOGLE_OAUTH_URI = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${SCOPE}`;
+export const GOOGLE_OAUTH_URI = `https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${SCOPE}`;
 
 
 //kakao
-const KAKAO_CLIENT_ID = "b05cd2c325a6d15c882233502e21a1fc"; //REST API KEY (CLIENT ID)
+const KAKAO_CLIENT_ID = "CLIENT ID"; //REST API KEY (CLIENT ID)
 const KAKAO_REDIRECT_URI =  "http://localhost:3000/oauth/callback/kakao";
 const KAKAO_LOGOUT_REDIRECT_URI = "http://localhost:3000";
 
