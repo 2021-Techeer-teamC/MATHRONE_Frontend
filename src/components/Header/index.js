b38f81ff4058bf15e5601cfafa2f10ecaf9f32ae
Yeonju Seo(서연주) <44168693+h01010@users.noreply.github.com>
2023-04-23 01:02:59 +0900

    (MR-97) Header prettier 적용

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,78 +1,85 @@
-import React, {useState} from "react";
-import PropTypes from "prop-types";
-import {Box, Grid } from "@mui/material/";
-import Button from "@mui/material/Button";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Box, Grid } from '@mui/material/';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 import Logo from '../Logo/index.tsx';
-import {CgProfile} from 'react-icons/cg';
-import "../../assets/styles/components.css";
-import "./style.css";
-import snsLoginService from "../../services/snsLoginService";
-import {KAKAO_LOGOUT_URL} from "../../pages/Oauth/OauthData";
+import { CgProfile } from 'react-icons/cg';
+import '../../assets/styles/components.css';
+import './style.css';
+import snsLoginService from '../../services/snsLoginService';
+import { KAKAO_LOGOUT_URL } from '../../pages/Oauth/OauthData';
 
 function Header() {
-  const [loginStatus, setLoginStatus] = useState(localStorage.getItem('accessToken')?true:false);
-  const [thirdParty, setThirdParty] = useState(localStorage.getItem('thirdParty'));
+  const [loginStatus, setLoginStatus] = useState(
+    localStorage.getItem('accessToken') ? true : false,
+  );
+  const [thirdParty, setThirdParty] = useState(
+    localStorage.getItem('thirdParty'),
+  );
 
-  const onLogoutClick = () =>{
-
+  const onLogoutClick = () => {
     localStorage.removeItem('accessToken');
     localStorage.removeItem('userId');
     localStorage.removeItem('thirdParty');
     localStorage.removeItem('snsAccessToken');
 
-
     setLoginStatus(false);
 
-    if(thirdParty === 'kakao') {
-        window.location.href = KAKAO_LOGOUT_URL; //카카오 로그아웃
+    if (thirdParty === 'kakao') {
+      window.location.href = KAKAO_LOGOUT_URL; //카카오 로그아웃
     }
-
-  }
-
+  };
 
   return (
-      <Box className="header-box" sx={{ flexGrow: 1 }}>
-        <div className="header">
-          <Logo/>
+    <Box className="header-box" sx={{ flexGrow: 1 }}>
+      <div className="header">
+        <Logo />
 
-          <Box sx={{ flexGrow: 1 }} />
+        <Box sx={{ flexGrow: 1 }} />
 
-          <Box sx={{ display: { xs: 'flex' } }}>
-            {!loginStatus? (
-              <Grid container spacing={1}>
-                <Grid item xs={6} md={7}>
-                  <Link to="/signup" className="header-link">
-                    <Button id="register-button">회원가입</Button>
-                  </Link>
-
-                </Grid>
-                <Grid item xs={6} md={5}>
-                  <Link to="/signin" className="header-link">
-                    <Button id="login-button">로그인</Button>
-                  </Link>
-                </Grid>
+        <Box sx={{ display: { xs: 'flex' } }}>
+          {!loginStatus ? (
+            <Grid container spacing={1}>
+              <Grid item xs={6} md={7}>
+                <Link to="/signup" className="header-link">
+                  <Button id="register-button">회원가입</Button>
+                </Link>
               </Grid>
-            ) :(
-              <Grid container spacing={1}>
-                <Grid item xs={6} md={5}>
-                  <Link to="/profile" style={{ textDecoration: 'none' }}>
-                    <CgProfile style={{fontSize:'36px', color:'#009688', margin:'auto'}}/>
-                  </Link>
-                </Grid>
-                <Grid item xs={6} md={7}>
-                  <Link to="/" style={{ textDecoration: 'none' }} onClick={onLogoutClick}>
-                    <Button id="login-button">로그아웃</Button>
-                  </Link>
-                </Grid>
+              <Grid item xs={6} md={5}>
+                <Link to="/signin" className="header-link">
+                  <Button id="login-button">로그인</Button>
+                </Link>
               </Grid>
-            )}
-          </Box>
-        </div>
-      </Box>
+            </Grid>
+          ) : (
+            <Grid container spacing={1}>
+              <Grid item xs={6} md={5}>
+                <Link to="/profile" style={{ textDecoration: 'none' }}>
+                  <CgProfile
+                    style={{
+                      fontSize: '36px',
+                      color: '#009688',
+                      margin: 'auto',
+                    }}
+                  />
+                </Link>
+              </Grid>
+              <Grid item xs={6} md={7}>
+                <Link
+                  to="/"
+                  style={{ textDecoration: 'none' }}
+                  onClick={onLogoutClick}
+                >
+                  <Button id="login-button">로그아웃</Button>
+                </Link>
+              </Grid>
+            </Grid>
+          )}
+        </Box>
+      </div>
+    </Box>
   );
 }
 
 export default Header;
-
-
