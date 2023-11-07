5cccf78f8e0ca8882ef1df69f217dbc76fe49713
Yeonju Seo(서연주) <44168693+h01010@users.noreply.github.com>
2023-11-07 16:50:24 +0900

    fix: problem 조회 api 주소 변경

--- src/services/problemsService.ts
+++ src/services/problemsService.ts
@@ -4,7 +4,7 @@ import axios from '../utils/axios';
 class problemsService {
   getProblems(workbookId: string | undefined, chapterId: string | undefined) {
     return axios.get<problemData[]>(
-      `${process.env.REACT_APP_IP}/problem/detail-page/all?workbookId=${workbookId}&chapterId=${chapterId}`, // api 주소
+      `${process.env.REACT_APP_IP}/problem?workbookId=${workbookId}&chapterId=${chapterId}`, // api 주소
       //"http://localhost:8080/problem/detail-page/all?workbookId=02&chapterId=01",
     );
   }
