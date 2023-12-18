dd2c50de83fbacc24b6c1d18c3f78c0452fcb340
Yeonju Seo(서연주) <44168693+h01010@users.noreply.github.com>
2023-12-18 18:29:51 +0900

    refactor: eslint 업데이트

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,45 @@
 module.exports = {
+  // root: true,
+  // parser: '@typescript-eslint/parser',
+  // plugins: ["@typescript-eslint", "prettier"],
+  // // extends: ['standard-with-typescript', 'prettier', 'plugin:prettier/recommended'],
+  // extends: [
+  //   'prettier',
+  //   'plugin:prettier/recommended',
+  //   "plugin:@typescript-eslint/parser"
+  // ],
+  // parserOptions: {
+  //   project: './tsconfig.json',
+  //   sourceType: 'module',
+  //   tsconfigRootDir: __dirname,
+  // },
+  // ignorePatterns: [".eslintrc.js"],
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
-  // extends: ['standard-with-typescript', 'prettier', 'plugin:prettier/recommended'],
-  extends: [
-    'prettier',
-    'plugin:prettier/recommended',
-    "plugin:@typescript-eslint/parser"
-  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
   parserOptions: {
-    project: ['tsconfig.json'],
-    createDefaultProgram: true,
+    project: "./tsconfig.json",
   },
+
+  env: {
+    node: true,
+  },
+
+  extends: [
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+
+  ignorePatterns: [".eslintrc.js"], 
   rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/semi': 'off',
     '@typescript-eslint/space-before-function-paren': 'off',
   },
-  ignorePatterns: ['.eslintrc.js'],
 };
