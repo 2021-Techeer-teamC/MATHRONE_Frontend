4886cd2e0b616bedf4fef28d6374126a76afecbd
Yeonju Seo(서연주) <44168693+h01010@users.noreply.github.com>
2023-12-18 17:51:13 +0900

    build: prettier & eslint 설치

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  // extends: ['standard-with-typescript', 'prettier', 'plugin:prettier/recommended'],
+  extends: ['prettier', 'plugin:prettier/recommended'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    '@typescript-eslint/semi': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+  },
+  ignorePatterns: ['.eslintrc.js'],
+};
