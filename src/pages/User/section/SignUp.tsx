7db87f292867c958ff626b967f6eb4e761258929
Yeonju Seo(서연주) <44168693+h01010@users.noreply.github.com>
2024-02-22 17:28:34 +0900

    refactor: line refactoring

--- src/pages/User/section/SignUp.tsx
+++ src/pages/User/section/SignUp.tsx
@@ -30,13 +30,35 @@ export default function SignUP() {
         <FormControl component="fieldset" variant="standard">
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              <TextField required autoFocus fullWidth type="string" id="accountId" name="accountId" label="사용자 ID" />
+              <TextField
+                required
+                autoFocus
+                fullWidth
+                type="string"
+                id="accountId"
+                name="accountId"
+                label="사용자 ID"
+              />
             </Grid>
             <Grid item xs={12}>
-              <TextField required autoFocus fullWidth type="email" id="email" name="email" label="이메일 주소" />
+              <TextField
+                required
+                fullWidth
+                type="email"
+                id="email"
+                name="email"
+                label="이메일 주소"
+              />
             </Grid>
             <Grid item xs={12}>
-              <TextField required fullWidth type="password" id="password" name="password" label="비밀번호 (숫자+영문자+특수문자 8자리 이상)" />
+              <TextField
+              required
+              fullWidth
+              type="password"
+              id="password"
+              name="password"
+              label="비밀번호 (숫자+영문자+특수문자 8자리 이상)"
+            />
             </Grid>
           </Grid>
           <Button id="signup_button" type="submit" fullWidth variant="contained" size="large">
